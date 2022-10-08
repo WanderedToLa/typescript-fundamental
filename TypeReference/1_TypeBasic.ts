8a7a03466962031ff65abc29d8413e2c34ef28da
WanderedToLa <rladntjr9808@gmail.com>
2022-10-08 19:56:19 +0900

    Update: TypeReference

--- TypeReference/1_TypeBasic.ts
+++ TypeReference/1_TypeBasic.ts
@@ -1,6 +1,23 @@
 // 문자열
 let str:string = 'hello';
 
+// 숫자
 let num:number = 10;
 
+// 배열
 let arr:Array<number> = [1 , 2 ,3];
+let Animals:Array<string> = ['cat' , 'dog' , 'tiger'];
+let items: number[] = [1 , 2 , 3];
+
+// 튜플 - 특정 인덱스의 타입을 지정
+let address: [string , number] = ['sangam' , 402];
+
+// 객체
+let obj:{} = {};
+let person: { name:string , age:number } = {
+    name: 'Brave Cookie',
+    age: 10,
+}
+
+// boolean
+let show:boolean = true;
