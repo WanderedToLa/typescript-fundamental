13f0dedf2bf819ad2321e5cfd6773dbfafe94142
WanderedToLa <rladntjr9808@gmail.com>
2022-12-16 12:18:12 +0900

    Fix : operator desc

--- TypeReference/5_operator.ts
+++ TypeReference/5_operator.ts
@@ -1,5 +1,5 @@
 /**
- * @description Union Type '|'
+ * @description Union Type '|'연산
  */
 
 function logMSG(value: string | number) {
@@ -9,13 +9,13 @@ logMSG("hi");
 logMSG(10);
 
 /**
- * @description Type Guard 특정 타입으로 타입의 범위를 좁힘
+ * @description typeGuard 특정 타입으로 타입의 범위를 좁힘
  */
 
 function typeGuard(value: string | number) {
   typeof value == "number" ? value.toLocaleString() : "";
   typeof value == "string" ? value.toString() : "";
-  throw new Error("Invalid Value");
+  throw new TypeError("Invalid Value");
 }
 typeGuard("hi");
 typeGuard(100);
