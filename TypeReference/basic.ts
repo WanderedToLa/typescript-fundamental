a5271820d3df2b5936328983f1dea63c185b45cd
WanderedToLa <rladntjr9808@gmail.com>
2023-01-16 20:39:12 +0900

    Update type basic

---
+++ TypeReference/basic.ts
@@ -0,0 +1,7 @@
+const a: string = "hello World";
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+// const f: symbol = Symbol.for("abc");
+// const g: bigint = 1_000_000n; ES2020 미만 x
