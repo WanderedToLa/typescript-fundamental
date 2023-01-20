e7f49fef5a1617a528069f6448905d849a146f58
WanderedToLa <rladntjr9808@gmail.com>
2023-01-21 00:35:54 +0900

    Update filter type

---
+++ detail/filter.ts
@@ -0,0 +1,9 @@
+interface FilterCustom<T> {
+  filter<U extends T>(callback: (value: T) => value is U): U[];
+}
+
+const filterArr: FilterCustom<number | string> = [1, "2", 3];
+const stringFilterArr: FilterCustom<string> = ["1", "2", "3"];
+
+const usingFilter = filterArr.filter((v): v is string => typeof v === "string");
+// const usingFilterMethod = filterArr.filter((v) => v.toString());
