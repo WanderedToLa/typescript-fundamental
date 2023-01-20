cf26de05c7dff5c43367a2a29b5969bb83f6cd60
WanderedToLa <rladntjr9808@gmail.com>
2023-01-21 00:38:08 +0900

    Fix filter conditon

--- detail/filter.ts
+++ detail/filter.ts
@@ -6,4 +6,4 @@ const filterArr: FilterCustom<number | string> = [1, "2", 3];
 const stringFilterArr: FilterCustom<string> = ["1", "2", "3"];
 
 const usingFilter = filterArr.filter((v): v is string => typeof v === "string");
-// const usingFilterMethod = filterArr.filter((v) => v.toString());
+const usingFilterMethod = filterArr.filter((v): v is number => v < 3);
