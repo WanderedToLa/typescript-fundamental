b64b6be189ea72ad2b6abd1d5accbb2cb5394d16
WanderedToLa <rladntjr9808@gmail.com>
2023-01-22 12:28:54 +0900

    Update 공변성

--- detail/filter.ts
+++ detail/filter.ts
@@ -7,3 +7,14 @@ const stringFilterArr: FilterCustom<string> = ["1", "2", "3"];
 
 const usingFilter = filterArr.filter((v): v is string => typeof v === "string");
 const usingFilterMethod = filterArr.filter((v): v is number => v < 3);
+
+// 더 넓은 타입에 대입가능
+
+function returnNumber(x: string): number {
+  return Number(x);
+}
+
+returnNumber("1"); // 1
+
+type returnNumberType = (x: string) => string | number;
+const returnNumberB: returnNumberType = returnNumber;
