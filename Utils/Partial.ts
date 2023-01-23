387b19e6ce325003d11213a10cee0cb22d2132a7
WanderedToLa <rladntjr9808@gmail.com>
2023-01-23 19:10:31 +0900

    Update Partial

--- Utils/Partial.ts
+++ Utils/Partial.ts
@@ -1,1 +1,34 @@
-// ..
+interface ProfileObject {
+  name: string;
+  age: number;
+  address: string;
+}
+
+const wooseok: ProfileObject = {
+  name: "wooseok",
+  age: 24,
+  address: "Ansan",
+};
+
+// interface ProfileObject {
+//   name?: string;
+//   age?: number;
+//   address?: string;
+// }
+
+/**
+ * P<ProfileObject> {
+ *   name? : ProfileObject['name']
+ *   age? : ProfileObject['age']
+ *   address? : ProfileObject['address']
+ * }
+ */
+
+type P<T> = {
+  [Key in keyof T]?: T[Key];
+};
+
+const newWooseok: P<ProfileObject> = {
+  name: "wooseok",
+  age: 24,
+};
