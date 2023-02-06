46e05865f1ad8025d20980e3802fa12b78ae184c
WanderedToLa <rladntjr9808@gmail.com>
2023-02-06 11:58:11 +0900

    Update Typechallenge pick

---
+++ type-challenges/Pick.ts
@@ -0,0 +1,20 @@
+/**
+ * `T`에서 `K` 프로퍼티만 선택해 새로운 오브젝트 타입을 만드는 내장 제네릭 `Pick<T, K>`을 이를 사용하지 않고 구현하세요.
+ */
+
+type MyPick<T, K extends keyof T> = {
+  [Key in K]: T[Key];
+};
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyPick<Todo, "title" | "completed">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
