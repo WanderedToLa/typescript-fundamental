d45b5ea734406122294f9d02aae661bf4e2037fe
WanderedToLa <rladntjr9808@gmail.com>
2023-02-06 17:41:42 +0900

    if type add extends boolean

--- type-challenges/if.ts
+++ type-challenges/if.ts
@@ -4,7 +4,7 @@
 
 import type { Equal, Expect } from "./utils";
 
-type If<C, T, F> = C extends true ? T : F;
+type If<C extends boolean, T, F> = C extends true ? T : F;
 
 type cases = [
   Expect<Equal<If<true, "a", "b">, "a">>,
