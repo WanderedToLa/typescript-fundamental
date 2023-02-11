f3a613039810f5a9a663274566713d2f001125e0
WanderedToLa <rladntjr9808@gmail.com>
2023-02-11 23:26:46 +0900

    add Comment Duck typing

--- detail/Promise.ts
+++ detail/Promise.ts
@@ -9,7 +9,7 @@ const promise3 = new Promise((res, rej) => {
 
 // all<T extends readonly unknown[] | []>(values: T): Promise<{ -readonly [P in keyof T]: Awaited<T[P]> }>;
 
-// type Awaited<T> =
+// type Awaited<T> = // Duck Typing
 //     T extends null | undefined ? T : // special case for `null | undefined` when not in `--strictNullChecks` mode
 //         T extends object & { then(onfulfilled: infer F, ...args: infer _): any } ? // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
 //             F extends ((value: infer V, ...args: infer _) => any) ? // if the argument to `then` is callable, extracts the first argument
