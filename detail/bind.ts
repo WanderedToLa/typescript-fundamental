b8bd4035a28722e14e24603d60512c30ed273a73
WanderedToLa <rladntjr9808@gmail.com>
2023-02-12 17:26:09 +0900

    add ThisParameterType analyze

--- detail/bind.ts
+++ detail/bind.ts
@@ -1,7 +1,12 @@
-function aBind(this : Window | typeof bindObj){
+function aBind(this : Window | typeof bindObj , param: string){
     console.log(this.name);
 }
 
 const bindObj = { name : 'wooseok'}
 const bBind = aBind.bind(bindObj)
 bBind() // 'wooseok'
+
+// type ThisParameterType<T> = T extends (this: infer U, ...args: never) => any ? U : unknown;
+// type OmitThisParameter<T> = unknown extends ThisParameterType<T> ? T : T extends (...args: infer A) => infer R ? (...args: A) => R : T;
+type T = ThisParameterType<typeof aBind> // Window | typeof bindObj
+type Nothis = OmitThisParameter<typeof aBind> // remove this and param: string
