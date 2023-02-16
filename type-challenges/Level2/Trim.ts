9b9401f7760a0eabc1fe2ef3ff50ebc95b42a775
WanderedToLa <rladntjr9808@gmail.com>
2023-02-16 15:23:29 +0900

    Fix lint

--- type-challenges/Level2/Trim.ts
+++ type-challenges/Level2/Trim.ts
@@ -8,16 +8,20 @@
 
 import type { Equal, Expect } from "../utils";
 
-type Space = ' ' | '\n' | '\t'
-type Trim<S extends string> = S extends `${Space}${infer Rest}` ? Trim<Rest> : S extends `${infer Rest}${Space}` ? Trim<Rest> : S;
+type Space = " " | "\n" | "\t";
+type Trim<S extends string> = S extends `${Space}${infer Rest}`
+  ? Trim<Rest>
+  : S extends `${infer Rest}${Space}`
+  ? Trim<Rest>
+  : S;
 
 type cases = [
-    Expect<Equal<Trim<'str'>, 'str'>>,
-    Expect<Equal<Trim<' str'>, 'str'>>,
-    Expect<Equal<Trim<'     str'>, 'str'>>,
-    Expect<Equal<Trim<'str   '>, 'str'>>,
-    Expect<Equal<Trim<'     str     '>, 'str'>>,
-    Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
-    Expect<Equal<Trim<''>, ''>>,
-    Expect<Equal<Trim<' \n\t '>, ''>>,
-]
+  Expect<Equal<Trim<"str">, "str">>,
+  Expect<Equal<Trim<" str">, "str">>,
+  Expect<Equal<Trim<"     str">, "str">>,
+  Expect<Equal<Trim<"str   ">, "str">>,
+  Expect<Equal<Trim<"     str     ">, "str">>,
+  Expect<Equal<Trim<"   \n\t foo bar \t">, "foo bar">>,
+  Expect<Equal<Trim<"">, "">>,
+  Expect<Equal<Trim<" \n\t ">, "">>
+];
